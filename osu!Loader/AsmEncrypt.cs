3055167985b962c2c4bbe4c7373fe0af328e2d2b
Starlet Lockhomes <gpyron@mail.com>
2019-03-27 16:26:13 -0400

    Oops, fix aligning.

--- osu!Loader/AsmEncrypt.cs
+++ osu!Loader/AsmEncrypt.cs
@@ -3,19 +3,19 @@
     class AsmEncrypt
     {
         // Class
-        public const string class_OsuMain             = "#=zLrnD1eEiOf2qQaQ0Bg==";     // osu
-        public const string class_pWebRequest         = "#=zJhSqLsBKnm$KCdddCnPVpGA="; // osu_common.Helpers
-        public const string class_BanchoClient        = "#=z34NKAQKveBlsNFZ4ZnVrK98="; // osu.Online
+        public const string class_OsuMain      = "#=zLrnD1eEiOf2qQaQ0Bg==";     // osu
+        public const string class_pWebRequest  = "#=zJhSqLsBKnm$KCdddCnPVpGA="; // osu_common.Helpers
+        public const string class_BanchoClient = "#=z34NKAQKveBlsNFZ4ZnVrK98="; // osu.Online
 
         // Field
         public const string field_OsuMain_startupValue = "#=zWAvH2v8=";
         public const string field_pWebRequest_url      = "#=zik1dj1c=";
 
         // Method
-        public const string method_OsuMain_FullPath                = "#=zWoxncngwrX5q";
-        public const string method_OsuMain_Filename                = "#=zfYoCJY_eoxFn";
-        public const string method_pWebRequest_set_Url             = "#=zpUsWV4ro_q6J";
-        public const string method_pWebRequest_checkCertificate    = "#=zngvtDQZIX2QF";
-        public const string method_BanchoClient_SetServer          = "#=zr5_X9_0=";
+        public const string method_OsuMain_FullPath             = "#=zWoxncngwrX5q";
+        public const string method_OsuMain_Filename             = "#=zfYoCJY_eoxFn";
+        public const string method_pWebRequest_set_Url          = "#=zpUsWV4ro_q6J";
+        public const string method_pWebRequest_checkCertificate = "#=zngvtDQZIX2QF";
+        public const string method_BanchoClient_SetServer       = "#=zr5_X9_0=";
     }
 }
